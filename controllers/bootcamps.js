0050f30f01457d0bdaacca00224c6eb822acd528
Prince Ajuobi <princekelsey299@gmail.com>
2020-01-22 01:08:56 +0200

    added advanced querring for sorting, selecting and pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,18 +8,60 @@ const asyncHandler = require("../middleware/async");
 // @access  Public
 exports.getAllBootCamps = asyncHandler(async (req, res, next) => {
   let querry;
-  let querryStr = JSON.stringify(req.query);
+  const reqQuery = { ...req.query };
+  const removeFields = ["select", "sort", "page", "limit"];
+  removeFields.forEach(field => delete reqQuery[field]);
+  let querryStr = JSON.stringify(reqQuery);
   querryStr = querryStr.replace(
     /\b(gt|gte|lt|lte|in)\b/g,
     match => `$${match}`
   );
 
   querry = BootCamp.find(JSON.parse(querryStr));
+  // select querry
+  if (req.query.select) {
+    const fileds = req.query.select.split(",").join(" ");
+    querry = querry.select(fileds);
+  }
+
+  // sort querry
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    querry = querry.sort(sortBy);
+  } else {
+    querry = querry.sort("-createdAt");
+  }
+
+  // pagination querry
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 30;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await BootCamp.countDocuments();
+  querry = querry.skip(startIndex).limit(limit);
+
   const bootCamps = await querry;
+
+  // pagination
+  const pagination = {};
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    };
+  }
 
   res.status(200).json({
     success: true,
     count: bootCamps.length,
+    pagination,
     data: bootCamps
   });
 });
