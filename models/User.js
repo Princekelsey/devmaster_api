7b9e1ce5815c20ad97dd3237d195abc9991e6506
Prince Ajuobi <princekelsey299@gmail.com>
2020-05-02 11:34:34 +0300

    added password reset, and user data update

--- models/User.js
+++ models/User.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
+const crypto = require("crypto");
 const bycrpt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const USER_SCHEMA = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Please add a name"]
+    required: [true, "Please add a name"],
   },
   email: {
     type: String,
@@ -13,45 +14,63 @@ const USER_SCHEMA = new mongoose.Schema({
     unique: true,
     match: [
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Please add a valid email"
-    ]
+      "Please add a valid email",
+    ],
   },
   role: {
     type: String,
     enum: ["user", "publisher"],
-    default: "user"
+    default: "user",
   },
   password: {
     type: String,
     required: [true, "Please add a password"],
     minlength: 6,
-    select: false
+    select: false,
   },
   resetPasswordToken: String,
   resetPasswordExpire: Date,
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
 // encrypt password
-USER_SCHEMA.pre("save", async function(next) {
+USER_SCHEMA.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
   const salt = await bycrpt.genSalt(10);
   this.password = await bycrpt.hash(this.password, salt);
   next();
 });
 
 // sign jwt and return
-USER_SCHEMA.methods.signJWTandReturn = function() {
+USER_SCHEMA.methods.signJWTandReturn = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE
+    expiresIn: process.env.JWT_EXPIRE,
   });
 };
 
 // compare login password with hashed password in database
-USER_SCHEMA.methods.matchPassword = async function(loginPassword) {
+USER_SCHEMA.methods.matchPassword = async function (loginPassword) {
   return await bycrpt.compare(loginPassword, this.password);
+};
+
+// generate and hash password token
+USER_SCHEMA.methods.getResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
 };
 
 module.exports = mongoose.model("User", USER_SCHEMA);
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,13 @@
 const express = require("express");
-const { registerUser, loginUser, currentUser } = require("../controllers/auth");
+const {
+  registerUser,
+  loginUser,
+  currentUser,
+  resetPassword,
+  forgotPassword,
+  updateDetails,
+  updatePassword,
+} = require("../controllers/auth");
 const { protectRoute } = require("../middleware/auth");
 
 const router = express.Router();
@@ -7,5 +15,9 @@ const router = express.Router();
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/me").get(protectRoute, currentUser);
+router.route("/updatedetails").put(protectRoute, updateDetails);
+router.route("/updatepassword").put(protectRoute, updatePassword);
+router.route("/forgotpassword").post(forgotPassword);
+router.route("/resetpassword/:resettoken").put(resetPassword);
 
 module.exports = router;
